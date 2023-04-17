1b1f28102ff340bad55e230fb136f5caf749f893
HeilanElf <3263718994@qq.com>
2023-04-17 03:49:55 +0000

    add

---
+++ aiceshi.dart
@@ -0,0 +1,13 @@
+void main() {
+  int x = 42;
+  
+  if (x > 50) {
+    print("x is greater than 50");
+  } 
+  else if (x == 50) {
+    print("x is equal to 50");
+  }
+  else {
+    print("x is less than 50");
+  }
+}
